4cb0687166e9faef9feaee96e95a9208b20a95f0
Anouk Van Bogaert <vb.anouk@gmail.com>
2022-08-28 06:45:20 +0000

    Use function to determine if number is odd or not

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function add(x, y) {
+  return x + y;
+}
+
+function divideByTwo(num) {
+  return num / 2;
+}
+
+var sum = add(5, 7);
+var average = divideByTwo(sum);
---
+++ whichschool.js
@@ -0,0 +1,11 @@
+const whichSchool = function (age) {
+  // Your code in here ...
+  if (age < 13) {
+    console.log("Elementary School");
+  } else if (age >= 13 && age <= 18) {
+    console.log("Secondary School");
+  } else if (age > 18) {
+    console.log("Lighthouse Labs")
+  }
+}
+console.log(whichSchool(2))
